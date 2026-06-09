a76da3b2289b63167cac92e489f83ddc4ca68411
pari25csu395-sketch <pari25csu395@ncuindia.edu>
2026-06-10 01:06:33 +0530

    Add files via upload

---
+++ list_of_movies_from_user.py
@@ -0,0 +1,6 @@
+movie1 = input("Enter the name of the first movie: ")
+movie2 = input("Enter the name of the second movie: ")
+movie3 = input("Enter the name of the third movie: ")
+
+list_of_movies = [movie1 , movie2 , movie3 ]
+print("The list of the fav movies of the user is", list_of_movies)

41d8a3167fc384fab935c6bd0e747780fe134f52
pari25csu395-sketch <pari25csu395@ncuindia.edu>
2026-06-09 12:31:11 +0530

    Add files via upload

---
+++ input.py
@@ -0,0 +1,7 @@
+name = input("What is your name? : ")
+age = int(input("What is your age? : "))
+marks = float(input("What are your marks? : "))
+
+print("Welcome ", name)
+print("Your age is ", age)
+print("Your marks are ", marks)
---
+++ type_conv_casting.py
@@ -0,0 +1,12 @@
+#type conversion
+#a = "100"
+#b = 4.25
+#print(a + b) # This will raise a TypeError because we cannot add a string and a float together.
+
+# type casting
+a = "100"
+b = 4.25
+a = int(a) #converting string to int.
+
+print (a + b)
+

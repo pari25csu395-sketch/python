c9285d84b5bfbc068fc43a533030affea0558225
pari25csu395-sketch <pari25csu395@ncuindia.edu>
2026-06-09 12:30:07 +0530

    Add files via upload

---
+++ average_of_2no.py
@@ -0,0 +1,7 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+sum = num1 +num2
+average = sum/2
+
+print("The average of the two numbers is: ",average)

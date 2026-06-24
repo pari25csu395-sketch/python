a712b0efee9c71511ca856ca5fdc1c3dbaef5c89
pari25csu395-sketch <pari25csu395@ncuindia.edu>
2026-06-24 14:11:48 +0530

    Add files via upload

---
+++ marks_of_sub_in_dict.py
@@ -0,0 +1,8 @@
+dict = {}
+
+chem = int(input("Enter the marks of chemistry: ") )
+phy = int(input("Enter the marks of physics: ") ) 
+maths = int(input("Enter the marks of maths: ") )
+
+dict = {"chemistry" : chem , "physics" : phy, "maths" : maths}
+print("The marks of the student in different subjects are: ", dict )

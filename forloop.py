f6935053a18547441ec66e7280e45f618c9da34d
pari25csu395-sketch <pari25csu395@ncuindia.edu>
2026-06-26 13:22:12 +0530

    Add files via upload

---
+++ forloop.py
@@ -0,0 +1,22 @@
+str = "pariaggarwal"
+for char in str:
+    print(char)
+
+
+
+
+list =[1,4,9,16,25,36,49,64,81,100]
+for el in list:
+    print(el)
+
+
+list =[1,4,9,16,25,36,49,64,81,100,64]
+x = 64
+idx = 0
+for el in list:
+    if(el == x):
+        print("number found at idx", idx)
+       
+    idx +=1
+        
+

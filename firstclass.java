7d4a814b1a2896c7cddd1131c484c817e645efca
pari25csu395-sketch <pari25csu395@ncuindia.edu>
2026-07-31 00:48:30 +0530

    Add files via upload

---
+++ firstclass.java
@@ -0,0 +1,7 @@
+class firstclass{
+    public static void main(String args[]){
+        System.out.println("hello world with java");
+        System.out.print("This is my first time on java\n");
+        System.out.println("hope it goes well");
+    }
+}

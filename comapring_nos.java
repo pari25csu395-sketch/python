770efaa4a232443b013ba344a3c8d9fea8398a87
pari25csu395-sketch <pari25csu395@ncuindia.edu>
2026-07-31 00:48:56 +0530

    Add files via upload

---
+++ comapring_nos.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class comapring_nos {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        if (x==y){
+            System.out.println("equal");
+        }
+
+        else if(x>y){
+            System.out.println("X IS GREATER");      
+            }
+            else{
+                System.out.println("y is greater");
+            }
+        }
+        
+
+         }
+        
+
+    
+    
+

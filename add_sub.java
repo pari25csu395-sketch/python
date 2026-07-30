bb89a0b3bedfb1beb1d2470b73020ad001c3c53b
pari25csu395-sketch <pari25csu395@ncuindia.edu>
2026-07-31 00:49:09 +0530

    Add files via upload

---
+++ add_sub.java
@@ -0,0 +1,11 @@
+public class add_sub {
+    public static void main(String args[]){
+        int a = 26;
+        int b = 56;
+        int sum = a+b;
+        int diff = b-a;
+        System.out.println(sum);
+        System.out.println(diff);
+    }
+    
+}
